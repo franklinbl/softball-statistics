2093d3aafacb5af83646cd41f3c897bf0b3a9593
franklin barco <franklinbarco98@gmail.com>
2025-03-07 18:36:54 -0400

    Be fair general and navbar style

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -27,63 +27,41 @@ const MenuBar: React.FC = () => {
   ];
 
   return (
-    <nav className="bg-white dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
-        <a className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="./src/assets/caimanes.png" className="h-20" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Caimanes</span>
-        </a>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          {user && (
-            <span>
-              <button
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                onClick={handleLogout}
-              >
-                Cerrar sesión
-              </button>
-            </span>
-          )}
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
+    <nav className="bg-white w-full z-20 top-0 start-0 border-b border-gray-200">
+      <div className="max-w-screen-xl mx-auto grid grid-cols-3 items-center">
+        <div className="flex items-center space-x-3 rtl:space-x-reverse">
+          <a className="flex items-center">
+            <img src="./src/assets/caimanes.png" className="h-20" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Caimanes</span>
+          </a>
         </div>
-        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-          <ul className="flex flex-col p-2 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+
+        <div className="flex justify-center">
+          <ul className="flex flex-col md:flex-row md:space-x-8 rtl:space-x-reverse font-medium">
             {menuItems
               .filter((item) => !item.requiresAuth || (item.requiresAuth && user)) // Filters items based on authentication status
               .map((item) => (
                 <li key={item.path}>
                   <Link
                     to={item.path}
-                    className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                    className="block py-2 px-3 text-black rounded bg-transparent"
                   >
                     {item.label}
                   </Link>
                 </li>
               ))}
           </ul>
+        </div>
+
+        <div className="flex justify-end">
+          {user && (
+            <button
+              className="rounded-xl bg-white border border-[#49a35c] text-[#49a35c] font-medium px-4 py-2 rounded hover:bg-[#49a35c] hover:text-white focus:outline-none focus:bg-[#49a35c] focus:text-white focus:border-[#49a35c] transition-colors"
+              onClick={handleLogout}
+            >
+              Cerrar sesión
+            </button>
+          )}
         </div>
       </div>
     </nav>
--- src/components/TournamentsTable.tsx
+++ src/components/TournamentsTable.tsx
@@ -14,7 +14,7 @@ const TournamentsTable: React.FC<TournamentsTableProps> = ({ tournaments, action
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                   <th scope="col" className="px-6 py-3">
-                      Nombre del jugador
+                      Nombre del torneo
                   </th>
                   <th scope="col" className="px-6 py-3">
                       Fecha de inicio
